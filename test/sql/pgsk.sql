5dbeeb05d8947e9e0d8ee2e6036b5311484bde0c
Julien Rouhaud <julien.rouhaud@free.fr>
2018-07-28 12:56:28 +0200

    Fix regression tests for older pg_stat_statements version

--- test/sql/pgsk.sql
+++ test/sql/pgsk.sql
@@ -6,9 +6,9 @@ SELECT 1 AS dummy;
 
 SELECT count(*) FROM pg_stat_kcache WHERE datname = current_database();
 
-SELECT count(*) FROM pg_stat_kcache_detail WHERE datname = current_database() AND query = 'SELECT $1 AS dummy';
+SELECT count(*) FROM pg_stat_kcache_detail WHERE datname = current_database() AND (query = 'SELECT $1 AS dummy' OR query = 'SELECT ? AS dummy;');
 
-SELECT query, reads, reads_blks, writes, writes_blks FROM pg_stat_kcache_detail WHERE datname = current_database() AND query = 'SELECT $1 AS dummy';
+SELECT reads, reads_blks, writes, writes_blks FROM pg_stat_kcache_detail WHERE datname = current_database() AND (query = 'SELECT $1 AS dummy' OR query = 'SELECT ? AS dummy;');
 
 -- dummy table
 CREATE TABLE test AS SELECT i FROM generate_series(1, 1000) i;
@@ -16,4 +16,4 @@ CREATE TABLE test AS SELECT i FROM generate_series(1, 1000) i;
 -- dummy query again
 SELECT count(*) FROM test;
 
-SELECT user_time + system_time > 0 AS cpu_time_ok FROM pg_stat_kcache_detail WHERE datname = current_database() AND query = 'SELECT count(*) FROM test';
+SELECT user_time + system_time > 0 AS cpu_time_ok FROM pg_stat_kcache_detail WHERE datname = current_database() AND query LIKE 'SELECT count(*) FROM test%';
